ac8df1aa3a70c5ec38d2a2d0e5e70ab15db5d8ad
toandeptrai12 <101626892+toandeptrai12@users.noreply.github.com>
2022-03-16 15:04:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,16 @@
 const CONFIG = {
     titleWeb: "Ấn vào thì biếc",
     introTitle: 'Quỳnh Cute',
-    introDesc: `☝đây là số 1
-✌️đây là số 2
-còn cậu là số phận cuộc đời tớ❤️`,
+    introDesc: `Nếu mà không phải cậu thì không phải bất cứ ai trên đời`,
     btnIntro: '^^HiHi^^',
     title: 'Cảm ơn cậu đã ở bên mình 🥰',
-    desc: 'Cậu cả đời này sẽ mãi là người của mình',
+    desc: 'Cậu chính là người con gái mà tớ yêu thương nà',
   
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: ' sao cậu lại thích mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'vì cậu đẹp trai,hihi <33333333',
+    reply: 'vì cậu đẹp trai,hihi <3',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hết dịch đi chơi nhaaa.',
     btnAccept: 'Okiiiii lun <3',
