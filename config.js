d682787e84d23534abf0ddcee69822c9627d70f8
toandeptrai12 <101626892+toandeptrai12@users.noreply.github.com>
2022-03-16 08:39:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,16 @@ const CONFIG = {
 ✌️đây là số 2
 còn cậu là số phận cuộc đời tớ❤️`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Cảm ơn cậu đã ở bên mình 🥰',
+    desc: 'Cậu cả đời này sẽ mãi là người của mình',
+  
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu mình <3',
+    question: ' sao cậu lại thích mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'vì cậu đẹp trai,hihi <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Hết dịch đi chơi nhaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD:https://www.facebook.com/hien.toan.0103
+    messLink: 'https://www.facebook.com/hien.toan.0103' //link mess của các bạn. VD:https://www.facebook.com/hien.toan.0103
 }
